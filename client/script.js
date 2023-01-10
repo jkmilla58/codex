9423cc0669ca419bb87381d93a101618f65d6755
jkmilla58 <justin@turvy.net>
2023-01-10 13:12:34 +1100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-tawny.vercel.app/', {
+    const response = await fetch('https://codex-1en921bqs-jkmilla58.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
